6dd1ef0d4c56749fc8e6bb3782f4806fe6299424
ArturPawelski <arturpawelski13@gmail.com>
2023-05-27 22:57:01 +0200

    adding regex

--- src/components/schema.tsx
+++ src/components/schema.tsx
@@ -1,7 +1,13 @@
 import { z } from 'zod';
 
+const regex = /^[^0-9]+$/;
+
 export const pizzaSchema = z.object({
-  name: z.string().min(2).max(40),
+  name: z
+    .string()
+    .min(3)
+    .max(40)
+    .refine((value) => regex.test(value), 'The name can only consist of letters.'),
   preparation_time: z.string().min(1).max(9),
   type: z.literal('pizza'),
   no_of_slices: z.number().int().min(2).max(20),
@@ -9,14 +15,22 @@ export const pizzaSchema = z.object({
 });
 
 export const soupSchema = z.object({
-  name: z.string().min(3).max(40),
+  name: z
+    .string()
+    .min(3)
+    .max(40)
+    .refine((value) => regex.test(value), 'The name can only consist of letters.'),
   preparation_time: z.string().min(1).max(9),
   type: z.literal('soup'),
   spiciness_scale: z.number().int().min(1).max(10),
 });
 
 export const sandwichSchema = z.object({
-  name: z.string().min(3).max(40),
+  name: z
+    .string()
+    .min(3)
+    .max(40)
+    .refine((value) => regex.test(value), 'The name can only consist of letters.'),
   preparation_time: z.string().min(1).max(9),
   type: z.literal('sandwich'),
   slices_of_bread: z.number().int().min(1).max(20),
